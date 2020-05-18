d009019e2e1c6a62c365b928a6fd2035aa36b918
Liviu Bouruc <bouruc.liviu2000@gmail.com>
2020-05-18 12:20:02 +0300

    backup

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,6 +1,5 @@
-import { listenerCount } from "cluster";
-
 var modalSettings = document.getElementById("modalSettings");
+var modalView = document.getElementById("modalView");
 var bugList = document.getElementById("bug-list");
 
 function checkGreeting() {
@@ -9,16 +8,6 @@ function checkGreeting() {
     else document.getElementById("greeting").textContent = "Welcome back, " + window.localStorage.getItem("username");
 }
 window.onload = checkGreeting();
-
-function openSettings() {
-    modalSettings.style.display = "block";
-    document.body.style.position = "fixed";
-}
-
-function closeModal() {
-    document.body.style.position = "absolute";
-    modalSettings.style.display = "none";
-}
 
 function saveSettings() {
     var Nume = document.getElementById("user-name").value;
@@ -30,6 +19,48 @@ function saveSettings() {
     closeModal();
 }
 
+function openSettings() {
+    modalSettings.style.display = "block";
+    document.body.style.position = "fixed";
+}
+
+function editViewModal(id) {
+    let modal = document.getElementById("modalView-info");
+    while(modal.firstChild) modal.removeChild(modal.firstChild);
+
+    let modalTitle = document.createElement('h1');
+    modalTitle.innerText = "Edit bug";
+    modalTitle.classList.add("modal-title");
+
+    let auxP1 = document.createElement('p');
+    auxP1.innerText = "Bug Title";
+
+    
+}
+
+function openView(id) {
+    modalView.style.display = "block";
+    document.body.style.positon = "fixed";
+
+    const res = fetch('/bugs/' + id)
+        .then((res) => { return res.json() })
+        .then((bug) => {
+            document.getElementById("modalView-title").textContent = bug.title;
+            document.getElementById("modalView-descript").textContent = bug.description;
+            document.getElementById("editBug").addEventListener('click', function() {
+                editViewModal(id);
+            });
+        });
+
+}
+
+function closeModal() {
+    document.body.style.position = "absolute";
+    modalSettings.style.display = "none";
+    modalView.style.display = "none";
+}
+
+
 function appendToDOM(bugs) {
     while(bugList.firstChild) bugList.removeChild(bugList.firstChild);
     
@@ -37,6 +68,9 @@ function appendToDOM(bugs) {
         let bugTitle = document.createElement('div');
         bugTitle.innerText = bugs[i].title;
         bugTitle.classList.add("bug-cap");
+        bugTitle.addEventListener('click', function() {
+            openView(bugs[i].id);
+        });
 
         let solveBtn = document.createElement('button');
         solveBtn.innerText = 'Solved';
@@ -55,7 +89,7 @@ function appendToDOM(bugs) {
 }
 
 function getBugs() {
-    fetch('https://localhost:3000/bugs')
+    fetch('/bugs')
         .then(function(response) {
             response.json().then(function(bugs) {
                 appendToDOM(bugs);
@@ -65,13 +99,13 @@ function getBugs() {
 
 function postBug() {
     let bugTitle = document.getElementById("b-title").value;
-    let bugDesc = document.getElementById("bug-descript").value;
+    let bugDesc = document.getElementById("b-descript").value;
     const bug = {
         title: bugTitle,
         description: bugDesc
     };
 
-    fetch('https://localhost:3000/bugs', {
+    fetch('/bugs', {
         method: 'post',
         headers: {
             "Content-type": "application/json"
@@ -83,7 +117,7 @@ function postBug() {
 }
 
 function deleteBug(id) {
-    fetch('https://localhost:3000/bugs/${id}', {
+    fetch('http://localhost:3000/bugs/${id}', {
         method: 'DELETE'
     }).then(function() {
         getBugs();
